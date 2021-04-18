b607e6fe2390becad03026abf64e7fb23110a0c3
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2021-04-18 01:37:45 -0300

    recupero variables en el front

--- src/components/cantidadDeUsuarios.js
+++ src/components/cantidadDeUsuarios.js
@@ -1,24 +1,24 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-class productosEnStock extends React.Component {
+class cantidadDeUsuarios extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             loading: true,
-            productos: {}
+            usuarios: {}
         }
     }
 
     componentDidMount = () => {
-        fetch("https://pintame.herokuapp.com/apis/productos")
+        fetch("https://pintame.herokuapp.com/apis/usuarios")
             .then(response => response.json())
             .then(data => {
                 console.log(data)
                 this.setState({
                     isLoaded: true,
-                    productos: data.meta
+                    usuarios: data.meta
                 })
             })
             .catch(error =>{
@@ -36,19 +36,19 @@ class productosEnStock extends React.Component {
 render(){
     
         console.log("estoy renderizando prod");
-        const { error, isLoaded, productos } = this.state;
+        const { error, isLoaded, usuarios } = this.state;
        
         return (
             <div class="col-md-4 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> Cantidad de Productos en Stock</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{productos.total}</div>
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Cantidad de Usuarios</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{usuarios.total}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
@@ -63,4 +63,4 @@ render(){
    
 
     }
-    export default productosEnStock;
+    export default cantidadDeUsuarios;
